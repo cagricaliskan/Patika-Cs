f616e2014a4af70494ee544bbee2cef95dd3e07b
Cagri Caliskan <caliskan1998@gmail.com>
2021-05-11 02:59:40 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Patika_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İsminizi giriniz");
+            string s = Console.ReadLine();
+            Console.WriteLine("Hoşgeldin " + s);
+
+            Console.ReadLine();
+
+        }
+    }
+}
