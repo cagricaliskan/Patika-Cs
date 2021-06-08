aed0d63a104d4ed8e29754f91996e2028a8adf81
Cagri Caliskan <caliskan1998@gmail.com>
2021-06-08 14:36:33 +0300

    ders 9.2

---
+++ donguler_while_foreach/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while 
+            // 1den başlayarak console'dan alınan sayıya kadar (sayı dahil) ortalama hesaplayıp console'a yazdıran program.
+            System.Console.WriteLine("Bir sayı giriniz.");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+
+            System.Console.WriteLine("Ortalama: " + (toplam / sayi));
+
+
+            //adan zye kadar olan sayıları konsole yazdır
+            char c = 'a';
+            while (c < 'z')
+            {
+                System.Console.WriteLine(c);
+                c++;
+            }
+
+
+
+            System.Console.WriteLine("***** FOREACH *****");
+
+            string[] arabalar = {"BMW" , "Ford", "Audi"};
+
+            foreach (var item in arabalar)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
