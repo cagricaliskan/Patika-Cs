5129cf1c6362291f32a0d3f263a82db02950ed1c
Muhammed Çağrı Çalışkan <caliskan1998@gmail.com>
2021-06-16 02:59:34 +0300

    odev 1 soru 2

---
+++ odev1/q2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[x];
+            
+            System.Console.WriteLine("{0} adet sayı giriniz.", x);
+            
+            for (int i = 0; i < x; i++)
+            {
+                Console.Write("{0}. sayi: ", i+1);
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            System.Console.WriteLine("Bolen degerini giriniz.");
+            int bolen = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("*** Bolene tam bolunen sayılar ***");
+            foreach (var item in numbers)
+            {
+                if(item%bolen == 0)
+                {
+                    System.Console.WriteLine(item);
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
