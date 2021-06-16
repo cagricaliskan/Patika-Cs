1feaca4072b72d9372ae8089ba30e014a9ab9def
Muhammed Çağrı Çalışkan <caliskan1998@gmail.com>
2021-06-16 03:04:52 +0300

    odev 1 soru 3

---
+++ odev1/q3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Kac kelime girmek istersiniz?");
+            int x = int.Parse(Console.ReadLine());
+
+            string[] kelimeler = new string[x];
+
+            for (int i = 0; i < x; i++)
+            {
+                System.Console.WriteLine("{0}. kelimeyi giriniz: ", i+1);
+                kelimeler[i] = Console.ReadLine();
+            }
+
+            System.Console.WriteLine("*** Girilen Kelimeler ***");
+            foreach (var item in kelimeler)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
