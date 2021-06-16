8c22d5425650c1cecfaf72026f9b49c4ff1dfafe
Muhammed Çağrı Çalışkan <caliskan1998@gmail.com>
2021-06-16 03:18:38 +0300

    odev 1 soru 4

---
+++ odev1/q4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a sentence");
+            string sen = Console.ReadLine();
+            int spacecount = 0;
+            int lettercount = 0;
+
+            for (int i = 0; i < sen.Length; i++)
+            {
+                if(char.IsWhiteSpace(sen[i])){
+                    spacecount++;
+                }
+            }
+
+            lettercount = sen.Length - spacecount;
+            
+            System.Console.WriteLine("Bu cümle {0} kelime ve {1} harften oluşuyor.", spacecount+1, lettercount);
+
+
+            Console.ReadLine();
+        }
+    }
+}
