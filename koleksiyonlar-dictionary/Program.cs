0dc6ae791d0e9877cad0c770b773627be215431b
Muhammed Çağrı Çalışkan <caliskan1998@gmail.com>
2021-06-17 03:25:23 +0300

    ders 14.3

---
+++ koleksiyonlar-dictionary/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace koleksiyonlar_dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> kullanıcılar = new Dictionary<int, string>();
+
+            kullanıcılar.Add(10,"Ayşe Yılmaz");
+            kullanıcılar.Add(12,"Ahmet Yılmaz");
+            kullanıcılar.Add(18,"Deniz Arda");
+            kullanıcılar.Add(20,"Özcan Coşar");
+
+            //Dizi elemanlarına erişim
+            System.Console.WriteLine("*** Elemanlara Erişim ***");
+            System.Console.WriteLine(kullanıcılar[12]);
+
+            foreach (var item in kullanıcılar)
+            {
+                System.Console.WriteLine(item);                
+            }
+
+
+            //contains
+            System.Console.WriteLine("*** Contains ***");
+
+            System.Console.WriteLine(kullanıcılar.ContainsKey(12));
+            System.Console.WriteLine(kullanıcılar.ContainsValue("Zikriye Ürkmez"));
+
+            //keys
+            System.Console.WriteLine("*** Keys ***");
+            foreach (var item in kullanıcılar.Keys)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            //values
+            System.Console.WriteLine("*** Values ***");
+            foreach (var item in kullanıcılar.Values)
+            {
+                System.Console.WriteLine(item);
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
